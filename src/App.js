08288014891de35bd8cfa4dfafc893c53e38bb81
carreguz13 <carreguz13@gmail.com>
2023-02-28 22:10:16 -0700

    starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react" /*importing the hook useEffect, The Effect Hook lets you perform side effects in function components.
+
+A quick example of side-effects would be:
+
+Fetch data from an API
+Manipulating the DOM.
+Mutate (change) state
+*/  
+/*
+1. No dependency passed:
+
+useEffect(() => {
+  //Runs on every render
+});
+
+2. An empty array:
+
+useEffect(() => {
+  //Runs only on the first render
+}, []);
+
+3. Props or state values:
+
+useEffect(() => {
+  //Runs on the first render
+  //And any time any dependency value changes
+}, [prop, state]);
+
+*/
 
 function App() {
+
+
+useEffect (() => {
+  
+  },[])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <h1>hola a todos</h1>
+    
+  )
 }
 
-export default App;
+export default App
