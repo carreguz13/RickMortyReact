785fdfb5151fca861111ea29beda41b16b55a19f
carreguz13 <carreguz13@gmail.com>
2023-03-07 23:11:28 -0700

    fetch url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import React, { useEffect } from "react";
 
 function App() {
+  const fetchCharacters = () => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
   return (
     <>
       <Navbar title="Rick And Morty App" />
